a3c4f330895c1497c9e1cc2d69d1d083d68666e4
Mayllon Veras <veras@ifpi.edu.br>
2021-03-01 10:00:02 -0300

    Projeto iniciado

---
+++ src/main/java/br/edu/ifpi/tedlive/TedLiveApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.tedlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TedLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TedLiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpi/tedlive/TedLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.tedlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TedLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
